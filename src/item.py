86f9217d0797a1cacb1f5b0f572417cfa564c928
Sam <Abdusamadov55@gmail.com>
2023-03-23 22:33:29 +0900

    Done Homework 13.2(test coverage 90%)

--- src/item.py
+++ src/item.py
@@ -1,3 +1,4 @@
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
@@ -12,10 +13,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            raise Exception('Name should be less than 10 characters')
+        else:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -30,3 +42,15 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('./src/items.csv', 'r') as csv_row:
+            file_reader = csv.DictReader(csv_row, delimiter=',')
+            for row in file_reader:
+                cls.all.append(row)
+
+    @staticmethod
+    def string_to_number(a):
+        b = float(a)
+        return int(b)
+
